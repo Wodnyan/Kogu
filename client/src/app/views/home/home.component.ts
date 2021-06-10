96722f3e05b130b614a6fcd0a1bb263629f3722d
Albert Gergo <gergoalbert.gta@gmail.com>
2021-06-10 21:55:12 +0300

    feat: auth service me, register methods

--- client/src/app/views/home/home.component.ts
+++ client/src/app/views/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { User } from "src/types";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  user: User | null = null;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.me().subscribe((resp) => {
+      if (resp["id"]) {
+        this.user = {
+          ...resp,
+          createdAt: resp.created_at,
+          updatedAt: resp.updated_at,
+        };
+      }
+    });
   }
-
 }
