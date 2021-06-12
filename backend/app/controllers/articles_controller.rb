c8d889cae3bffbf5995958de8846393bd7659e56
Albert Gergo <gergoalbert.gta@gmail.com>
2021-06-12 20:42:53 +0300

    feat: send author of article

--- backend/app/controllers/articles_controller.rb
+++ backend/app/controllers/articles_controller.rb
@@ -1,19 +1,36 @@
 class ArticlesController < ApplicationController
-  skip_before_action :authorize_request, only: %i[create show index]
+  skip_before_action :authorize_request, only: %i[show index user_articles]
   before_action :set_article, only: %i[show update destroy]
 
   def index
-    @article = Article.all
-    json_response(@article)
+    articles = Article.joins(:user).select(select)
+    mapped_articles = []
+
+    articles.each do |article|
+      structured = structure_article(article)
+      mapped_articles.push(structured)
+    end
+
+    json_response(mapped_articles)
+  end
+
+  # Get all articles of a user
+  def user_articles
+    articles = Article.where("user_id = #{params[:user_id]}").joins(:user).select(select)
+
+    mapped_articles = []
+
+    articles.each do |article|
+      structured = structure_article(article)
+      mapped_articles.push(structured)
+    end
+
+    json_response(mapped_articles)
   end
 
   def create
-    article_info = {
-      title: article_params[:title],
-      description: article_params[:description],
-      text: article_params[:text],
-      user_id: current_user[:id]
-    }
+    article_info = { title: article_params[:title], description: article_params[:description],
+                     text: article_params[:text], user_id: current_user[:id] }
     @article = Article.create!(article_info)
     json_response(@article, :created)
   end
@@ -34,6 +51,26 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.permit(:title, :description, :text, :user_id)
+    params.permit(:title, :description, :text)
+  end
+
+  def structure_article(article)
+    structuredArticle = {
+      id: article[:id],
+      title: article[:title],
+      description: article[:description],
+      text: article[:text],
+      updatedAt: article[:updated_at],
+      createdAt: article[:created_at],
+      author: {
+        id: article[:user_id],
+        name: article[:name],
+        email: article[:email]
+      }
+    }
+  end
+
+  def select
+    %w[id title description text created_at updated_at name user_id email]
   end
 end
--- backend/app/models/article.rb
+++ backend/app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates_presence_of :title, :description, :text
 
-  belongs_to :users, optional: true
+  belongs_to :user, optional: true
 end
