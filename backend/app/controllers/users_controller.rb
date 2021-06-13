c0d95bdd50df322b53b1f524cb16e7cf08c82e37
Albert Gergo <gergoalbert.gta@gmail.com>
2021-06-13 20:57:29 +0300

    feat: user resource

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
-  skip_before_action :authorize_request, only: %i[create]
+  skip_before_action :authorize_request, only: %i[signup show]
+  before_action :set_user
+
+  def show
+    json_response(@user)
+  end
 
   # Sign up
-  def create
+  def signup
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
     response = { message: Message.account_created, auth_token: auth_token }
@@ -22,5 +27,14 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  def select
+    # %w[id email name updated_at created_at]
+    ['id', 'email', 'name',  'updated_at as updatedAt', 'created_at as createdAt']
+  end
+
+  def set_user
+    @user = User.where('id = ?', params[:id]).select(select).first
   end
 end
