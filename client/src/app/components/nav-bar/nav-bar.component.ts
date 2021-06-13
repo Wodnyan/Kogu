63f4514de71fb1bd28699304172b087cb81c3e12
Albert Gergo <gergoalbert.gta@gmail.com>
2021-06-13 20:59:07 +0300

    feat: nav bar authenticated state

--- client/src/app/components/nav-bar/nav-bar.component.ts
+++ client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
+import { API_ENDPOINT_URL } from "src/constants";
+import { User } from "src/types";
 
 @Component({
   selector: "app-nav-bar",
@@ -6,7 +9,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./nav-bar.component.scss"],
 })
 export class NavBarComponent implements OnInit {
-  constructor() {}
+  user?: User;
+  constructor(authService: AuthService) {
+    authService.me().subscribe((res) => {
+      console.log(res);
+      if (res.id) {
+        this.user = {
+          ...res,
+          createdAt: res.created_at,
+          updatedAt: res.updated_at,
+        };
+      }
+    });
+  }
 
   ngOnInit(): void {}
+
+  get userPath() {
+    return `users/${this.user?.id}`;
+  }
 }
