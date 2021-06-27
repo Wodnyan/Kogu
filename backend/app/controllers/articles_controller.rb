1541f62fba1a31a6c574db6d93d37dcf050b1aea
Albert Gergo <gergoalbert.gta@gmail.com>
2021-06-27 21:15:02 +0300

    feat: format code, paginate users articles route

--- backend/app/controllers/articles_controller.rb
+++ backend/app/controllers/articles_controller.rb
@@ -15,9 +15,10 @@ class ArticlesController < ApplicationController
     json_response(mapped_articles)
   end
 
-  # Get all articles of a user
   def user_articles
-    articles = Article.where('user_id = ?', params[:user_id]).joins(:user).select(select)
+    page = request.query_parameters[:page] || 0
+    articles = Article.where('user_id = ?',
+                             params[:user_id]).order('articles.id DESC').joins(:user).select(select).limit(25).offset(page.to_i * 25)
 
     mapped_articles = []
 
@@ -30,26 +31,31 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    article_info = { title: article_params[:title], description: article_params[:description],
-                     text: article_params[:text], user_id: current_user[:id] }
+    article_info = {
+      title: article_params[:title],
+      description: article_params[:description],
+      text: article_params[:text],
+      user_id: current_user[:id]
+    }
     createdArticle = Article.create!(article_info)
-    @article = Article.where('articles.id = ?', createdArticle[:id]).joins(:user).select(select).first
+    @article =
+      Article.where('articles.id = ?', createdArticle[:id]).joins(:user).select(
+        select
+      )
+             .first
 
     json_response(structure_article(@article), :created)
   end
 
   def show
     if !@article
-      json_response({
-                      message: 'Not found'
-                    }, :not_found)
+      json_response({ message: 'Not found' }, :not_found)
     else
       json_response(structure_article(@article))
     end
   end
 
   def destroy
-    # Check if article belongs to user
     if @article && (current_user[:id] != @article[:user_id])
       raise(ExceptionHandler::AuthenticationError, Message.unauthorized)
     else
@@ -61,7 +67,9 @@ class ArticlesController < ApplicationController
   private
 
   def set_article
-    @article = Article.where('articles.id = ?', params[:id]).joins(:user).select(select).first
+    @article =
+      Article.where('articles.id = ?', params[:id]).joins(:user).select(select)
+             .first
   end
 
   def article_params
@@ -87,7 +95,18 @@ class ArticlesController < ApplicationController
   end
 
   def select
-    ['articles.id', 'title', 'description', 'text', 'articles.created_at as createdAt', 'articles.updated_at as updatedAt',
-     'name', 'user_id', 'email', 'users.created_at as userCreatedAt', 'users.updated_at as userUpdatedAt']
+    [
+      'articles.id',
+      'title',
+      'description',
+      'text',
+      'articles.created_at as createdAt',
+      'articles.updated_at as updatedAt',
+      'name',
+      'user_id',
+      'email',
+      'users.created_at as userCreatedAt',
+      'users.updated_at as userUpdatedAt'
+    ]
   end
 end
